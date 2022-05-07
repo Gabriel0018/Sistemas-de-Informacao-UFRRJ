0d5014d74ccbcc9bb3513f1ce4ebbce93b26b285
Gabriel Lima Prisco Madureira <gabrielmadureira9@gmail.com>
2022-05-07 16:48:11 -0300

    TXT - Implementação de um programa cadastral

---
+++ Lista 07 - 2.cpp
@@ -0,0 +1,330 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char nome[81];
+    int idade;
+    char curso[51];
+    float CR;
+    int matricula;
+}aluno;
+
+typedef struct{
+    aluno discente;
+    float notas;
+    int frequencia;
+}alunomat;
+
+typedef struct{
+    int codigo;
+    char nome_disciplina[81];
+    alunomat turma[40];
+}disciplina;
+
+void criar_disciplina(disciplina *aluno)
+{   
+	fflush(stdin);
+	FILE *file;
+	file = fopen("disciplinas.txt", "w");
+	
+	if (file == NULL){
+		printf("Erro! nao foi possivel abrir o arquivo!");
+		exit(1);
+	}
+    fflush(stdin);
+    printf("Digite o codigo da disciplina: "); scanf("%d", &aluno->codigo);
+    fflush(stdin);
+    printf("Digite o nome da disciplina: ");  fgets(aluno->nome_disciplina, 80, stdin);
+    fprintf(file, "disciplina: %sCodigo: %d", aluno->nome_disciplina, aluno->codigo);
+    fprintf(file, "\n\n");
+    int i;
+    for (i = 0; i < 40; i++){
+        aluno->turma[i].discente.matricula = 0;
+    }
+    printf("\n\n");
+    fclose(file);
+}
+
+void incluir_aluno(disciplina *aluno)
+{   
+	fflush(stdin);
+	FILE *file;
+	file = fopen("alunos.txt", "a");
+	
+	if (file == NULL){
+		printf("Erro! nao foi possivel abrir o arquivo!");
+		exit(1);
+	}
+	
+    int i, c = 0;
+    for (i; i < 40; i++){
+        if(aluno->turma[i].discente.matricula == 0){
+            printf("\nNome do aluno: "); fgets(aluno->turma[i].discente.nome, 80, stdin);
+            printf("Idade: "); scanf("%d", &aluno->turma[i].discente.idade); fflush(stdin);
+            printf("Nome do curso: "); fgets(aluno->turma[i].discente.curso, 50, stdin); 
+            printf("Informe o CR: "); scanf("%f", &aluno->turma[i].discente.CR); fflush(stdin);
+            printf("Matricula: "); scanf("%d", &aluno->turma[i].discente.matricula);
+            fprintf(file, "Discente = %sIdade: %d\n", aluno->turma[i].discente.nome, aluno->turma[i].discente.idade);
+			fprintf(file, "Curso: %sCR: %.2f\nMatricula: %d", aluno->turma[i].discente.curso, aluno->turma[i].discente.CR, aluno->turma[i].discente.matricula); 
+			fprintf(file, "\n\n"); 
+            printf("\n\n");
+            fclose(file);
+            break;
+        }
+        else{
+            c ++;
+        }
+        if (c == 40){
+            printf("A turma esta com sua capacidade maxima de discentes!");
+        }
+    }
+}
+void excluir_aluno(disciplina *aluno)
+{   
+	FILE *file;
+	file = fopen("alunos.txt", "w");
+	
+    int matricula_digitada, i, registrador = 0;
+    printf("Informe o numero de matricula do discente: "); scanf("%d", &matricula_digitada);
+    for (i = 0; i < 40; i++){
+        if (matricula_digitada == aluno->turma[i].discente.matricula){
+            aluno->turma[i].discente.matricula = 0;
+            printf("Aluno exluido da turma!");
+            registrador ++;
+            printf("\n\n");
+            
+            break;
+        }
+		fprintf(file, "Discente = %sIdade: %d\n", aluno->turma[i].discente.nome, aluno->turma[i].discente.idade);
+		fprintf(file, "Curso: %sCR: %.2f\nMatricula: %d", aluno->turma[i].discente.curso, aluno->turma[i].discente.CR, aluno->turma[i].discente.matricula); 
+		fprintf(file, "\n\n");
+    }
+    if (registrador == 0){
+    	printf("A matricula informada nao condiz com nenhum aluno com status ativo na turma");
+    	printf("\n\n");
+	}
+}
+
+void listar_alunos(disciplina *aluno)
+{	
+	FILE *file;
+	file = fopen("alunos.txt", "r");	
+	
+	if (file == NULL){
+		printf("Erro! nao foi possivel abrir o arquivo!");
+		exit(1);
+	}
+    int i;
+    char frase[1000];
+    for (i = 0; i < 40; i++){
+        if (aluno->turma[i].discente.matricula != 0){
+            while (fgets(frase, 1000, file)!= NULL){
+            	printf("%s", frase);
+			}
+        }
+    }
+    fclose(file);
+}
+
+void CR_medio(disciplina *aluno)
+{
+    float cr, total = 0, alu = 0;
+    int i;
+    
+    for(i = 0; i < 40; i++){
+        if(aluno->turma[i].discente.matricula != 0){
+        	alu++;
+            total += aluno->turma[i].discente.CR;
+        }  
+    }
+    if (alu == 0){
+    	printf("Nao ha discentes matriculados na turma para emitir a media do CR\n\n");
+	}
+	else{
+		cr = total / alu;
+    	printf("CR medio da turma = %.2f\n\n", cr);
+	}	
+}
+void atribuir_nota(disciplina *aluno)
+{   
+    int matricula_informada, i, registrador = 0;
+    printf("Informe a matricula do aluno: "); scanf("%d", &matricula_informada);
+    for(i = 0; i < 40; i++){
+        if (matricula_informada == aluno->turma[i].discente.matricula){
+            printf("Informe a nota final: "); scanf("%f", &aluno->turma[i].notas);
+            registrador ++;
+            printf("\n\n");
+            break;
+    	}
+    	
+    }
+    if (registrador == 0){
+    	printf("A matricula informada nao condiz com nenhum aluno com status ativo na turma");
+    	printf("\n\n");
+	}
+    
+}
+void atribuir_frequencia(disciplina *aluno)
+{
+    int matricula_informada, i, aulas, faltas, sub, registrador = 0;
+    float freq;
+    printf("Informe a matricula do aluno: "); scanf("%d", &matricula_informada);
+    for(i = 0; i < 40; i++){
+        if (matricula_informada == aluno->turma[i].discente.matricula){
+            printf("Informe a quantidade total de aulas: "); scanf("%d", &aulas);
+            printf("Informe quantas aulas o aluno faltou: "); scanf("%d", &faltas);
+            sub  = aulas - faltas;
+            freq = ((float) sub * 100) / aulas; 
+            aluno->turma[i].frequencia = freq;
+            registrador ++;
+            printf("\n\n");
+            break;
+    	}
+    }
+    if (registrador == 0){
+    	printf("A matricula informada nao condiz com nenhum aluno com status ativo na turma");
+    	printf("\n\n");
+	}
+}
+
+void imprimir_boletim(disciplina *aluno)
+{	
+	FILE *file;
+	file = fopen("boletins.txt", "w");
+	
+	if (file == NULL){
+		printf("Erro! nao foi possivel abrir o arquivo!");
+	}
+	else{
+		int i, cabecalho = 0;
+    	for (i = 0; i < 40; i++){
+        	if (aluno->turma[i].discente.matricula != 0){
+				printf("Discente = %sIdade: %d\n", aluno->turma[i].discente.nome, aluno->turma[i].discente.idade);
+				printf("Curso: %sCR: %.2f\nMatricula: %d\n", aluno->turma[i].discente.curso, aluno->turma[i].discente.CR, aluno->turma[i].discente.matricula);
+				printf("Nota: %.2f\nFrequencia: %d%%", aluno->turma[i].notas, aluno->turma[i].frequencia);
+				printf("\n\n");
+				
+				fprintf(file, "Discente = %sIdade: %d\n", aluno->turma[i].discente.nome, aluno->turma[i].discente.idade);
+				fprintf(file, "Curso: %sCR: %.2f\nMatricula: %d\n", aluno->turma[i].discente.curso, aluno->turma[i].discente.CR, aluno->turma[i].discente.matricula);
+				fprintf(file, "Nota: %.2f\nFrequencia: %d%%", aluno->turma[i].notas, aluno->turma[i].frequencia); 
+				fprintf(file, "\n\n");
+				
+			}	
+    	}
+	}
+	fclose(file);
+	printf("\n\n");
+}
+
+void fechar_disciplina(disciplina *aluno)
+{   
+    int i;
+    aluno->codigo = ' ';
+    strcpy(aluno->nome_disciplina," ");
+    for (i = 0; i < 40; i++){
+        if(aluno->turma[i].discente.matricula == 0){
+            break;
+        }
+        else{
+            aluno->turma[i].discente.matricula = 0;
+        }
+    }
+}
+
+int main(void)
+{	
+	remove("alunos.txt");
+	remove("boletins.txt");
+    int leitor, inicializar = 1;
+    disciplina aluno;
+    do
+    {   
+        if (inicializar == 1)
+        {
+            while(1){
+                if (leitor == 1){
+                    printf("----Criando disciplina----\n");
+                    criar_disciplina(&aluno);
+                    inicializar = 0;
+                    break;
+                }
+                else if (leitor == 2){
+                    exit(1);
+            }
+                printf("(1)Criar disciplina\n(2)Sair\n");
+                printf("Opcao: ");
+                scanf("%d", &leitor);
+            }
+        }
+        printf("(1)Criar disciplina\n(2)Incluir aluno\n(3)Excluir aluno\n");
+        printf("(4)Adicionar nota ao aluno\n(5)Registrar frequencia do aluno\n");
+        printf("(6)Listar alunos\n(7)Calcular CR da turma\n");
+        printf("(8)Imprimir boletim\n(9)Fechar disciplina\n(10)sair");
+        printf("\nOpcao: ");
+        scanf("%d", &leitor);
+        printf("\n");
+        
+        switch(leitor){
+            case 1:
+                printf("----Criando disciplina----\n");
+                criar_disciplina(&aluno);
+                break;
+            case 2:
+                printf("----Incluindo aluno----\n");
+                incluir_aluno(&aluno);
+                break;
+            case 3:
+                printf("----Excluindo aluno----\n");
+                excluir_aluno(&aluno);
+                break;
+            case 4:
+                printf("----Adicionando nota---\n");
+                atribuir_nota(&aluno);
+                break;
+            case 5:
+                printf("----Atribuindo frequencia----\n");
+                atribuir_frequencia(&aluno);
+                break;
+            case 6:
+                printf("----Listando alunos----\n");
+                listar_alunos(&aluno);
+                break;
+            case 7:
+                printf("----Calculando CR da turma----\n");
+                CR_medio(&aluno);
+                break;
+            case 8:
+                printf("----Imprimindo boletim----\n");
+                imprimir_boletim(&aluno);
+                break;
+            case 9:
+                printf("Disciplina fechada com sucesso!\n");
+                fechar_disciplina(&aluno);
+                while(1){
+                    if (leitor == 1){
+                        printf("----Criando disciplina----\n");
+                        criar_disciplina(&aluno);
+                        break;
+                    }
+                    else if (leitor == 2){
+                        exit(1);
+                    }
+                    printf("(1)Criar disciplina\n(2)Sair\n");
+                    printf("Opcao: ");
+                    scanf("%d", &leitor);
+                }
+            case 10:
+                break;
+                
+            default:
+                printf("Digite apenas uma das opções apresentadas!");
+                break;
+        }       
+    }
+    while(leitor != 10);
+	    
+    return 0;
+}
+
+
